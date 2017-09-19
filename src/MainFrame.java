988856c2a20e219f38164dae48ee9f828649d5df
jackjowChou <yungchen@gm.asia.edu.tw>
2017-09-19 12:53:05 +0800

    second commit

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,14 +1,43 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class MainFrame extends Frame{
+    private Button btnExit = new Button("Exit");
+    private Button btnAdd = new Button("Add");
+    private Label lab = new Label("0");
+    private int n = 0;
+
     public MainFrame(){
         initComp();
     }
     private void initComp(){
-        this.setLocation(100, 120);
-        this.setSize(400, 300);
+        this.setBounds(100, 120, 400, 300);
+        this.setLayout(null);
+        btnExit.setBounds(150, 110, 80,30);
+        btnAdd.setBounds(150, 80, 80,30);
+        lab.setBounds(150, 200, 80,30);
+        btnExit.setBackground(new Color(103, 39, 255));
+        btnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        btnAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                n++;
+                lab.setText(Integer.toString(n));
+            }
+        });
+
+        this.add(btnExit);
+        this.add(btnAdd);
+        this.add(lab);
+
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
